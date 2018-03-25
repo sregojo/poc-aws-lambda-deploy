f48bd709190d4fcb34e235a7cbdbf195585045d8
qpsseattle\santiago.regojo <santiago.regojo@valantmed.com>
2018-03-25 14:20:56 +0200

    Changes project structure

---
+++ lambdarest.api.console.host/Program.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace lambdarestapi
+{
+    /// <summary>
+    /// The Main function can be used to run the ASP.NET Core application locally using the Kestrel webserver.
+    /// </summary>
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost
+                .CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
